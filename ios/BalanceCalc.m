e0b62744059fdd3ceddcb0424a5ba295d7e1f462
Marcello Prattico <chimp@astrochimp.com>
2023-05-23 19:39:36 -0400

    setup native module for ios

---
+++ ios/BalanceCalc.m
@@ -0,0 +1,8 @@
+//
+//  BalanceCalc.m
+//  Ledger
+//
+//  Created by Marcello Prattico on 5/23/23.
+//
+
+#import <Foundation/Foundation.h>
---
+++ ios/BalanceCalc.swift
@@ -0,0 +1,8 @@
+//
+//  BalanceCalc.swift
+//  Ledger
+//
+//  Created by Marcello Prattico on 5/23/23.
+//
+
+import Foundation
