f292ffe35c4c161fa308309f486650a3351d3d49
Marcello Prattico <chimp@astrochimp.com>
2023-05-23 21:27:35 -0400

    add comments

--- ios/BalanceCalc.swift
+++ ios/BalanceCalc.swift
@@ -14,11 +14,23 @@ import Foundation
     callback: RCTResponseSenderBlock
   ) {
     var balanceTotal: Float = 0;
+    
+    struct LineItem:Decodable {
+      let amount: String
+    }
 
+    /*
+       Plan here is to use the transaction array and iterate to ge the amounts.
+       Swift does not seem to allow for accessing an object defined as <Any>/NSDictionary key/value
+     
+      Similar implementation works in Java/Android
+     */
+    
     if (transactions.count > 0) {
       for item in transactions {
         print(item)
-        // balanceTotal = balanceTotal + item
+        // Issue here with not being able to access the object keys or values. NSDictionary is Any type and cannot cast as LineItem
+        // balanceTotal = balanceTotal + item.amount
       }
     }
 
