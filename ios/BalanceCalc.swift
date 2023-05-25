4e041b6db11a4807b827ab4d6da252a8844dbf55
Marcello Prattico <chimp@astrochimp.com>
2023-05-25 09:11:01 -0400

    update extracting numbers correctly

--- ios/BalanceCalc.swift
+++ ios/BalanceCalc.swift
@@ -6,36 +6,34 @@
 //
 
 import Foundation
+import React
 
 @objc(BalanceCalc) class BalanceCalc: NSObject {
   @objc static func requiresMainQueueSetup() -> Bool { return true }
   @objc public func calculateBalance(
-    _ transactions: NSArray,
+    _ transactions: [NSDictionary],
     callback: RCTResponseSenderBlock
   ) {
-    var balanceTotal: Float = 0;
+    var balanceTotal: Double = 0;
     
     struct LineItem:Decodable {
-      let amount: String
+      let amount: Double
     }
 
     /*
-       Plan here is to use the transaction array and iterate to ge the amounts.
-       Swift does not seem to allow for accessing an object defined as <Any>/NSDictionary key/value
-     
-      Similar implementation works in Java/Android
+       Plan here is to use the transaction array and iterate to get the amounts.
+       Similar implementation works in Java/Android
      */
     
     if (transactions.count > 0) {
       for item in transactions {
-        print(item)
-        // Issue here with not being able to access the object keys or values. NSDictionary is Any type and cannot cast as LineItem
-        // balanceTotal = balanceTotal + item.amount
+        if let amount = item["amount"] as? Double {
+          let entry = LineItem(amount: amount)
+          balanceTotal = balanceTotal + amount
+        }
       }
     }
-
-
-    print(balanceTotal)
+    
     callback([balanceTotal])
   }
 }
